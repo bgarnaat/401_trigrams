b9b6123c3e62e56f00be669072a13ed4a696f69c
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-02 13:53:51 -0800

    renamed test to test_trigrams.  fixed pytest error

---
+++ test_trigrams.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""Tests for trigrams.py."""
+
+import pytest
+# import trigrams
+
+
+def test_pass():
+    """QC test pass check."""
+    assert 1 == 1
+
+
+def test_fail():
+    """QC test fail check."""
+    assert 1 == 0
