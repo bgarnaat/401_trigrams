0f344bd4059d616fa8f1d24c777a17a3dafc6867
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-02 16:23:55 -0800

    pre-upate test

--- trigrams.py
+++ trigrams.py
@@ -8,14 +8,19 @@ def read_file():
     """Open and read file input."""
     f = io.open('sherlock_small.txt', 'r')
     lines = ''.join(f.readlines())
+    lines = lines.replace('--', ' ')
+    print(lines)
     return lines
 
 
 def strip_punct(text):
     """Return string without punctuation."""
     for c in string.punctuation:
-        text = text.replace(c, ' ')
+        text = text.replace(c, '')
     return text
+
+
+# def make_dict()
 
 
 text = read_file()
