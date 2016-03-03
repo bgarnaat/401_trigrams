98956e8d3bd7f8698fa871337d64f29dcaea67e2
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-02 19:35:22 -0800

    making a damn story

--- trigrams.py
+++ trigrams.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 """Generate random story using trigrams."""
 import io
+import random
 import string
 
 
@@ -33,6 +34,24 @@ def create_dict(text):
         dict_key = (text[i], text[i + 1])
         word_dict.setdefault(dict_key, []).append(text[i + 2])
     return word_dict
+
+
+def make_a_damn_story(text, n):
+    """Make a damn story."""
+    primer = random.choice(list(text.keys()))
+    story = '{} {}'.format(primer[0], primer[1])
+    for i in range(0, n - 2):
+        pass
+
+
+# creat empty list.
+# assign value i and i + 1 as values equal to tuples in keys
+# append i+2 as value at key.
+
+# append i+3 as value of key made by i+1 and i+2
+# etc
+
+# smash story list into a string
 
 
 text = read_file()
