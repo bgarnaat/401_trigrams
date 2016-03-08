72377a348c621be4902fd25c4227324492a3d677
Ben Garnaat <garnaat.ben@gmail.com>
2016-03-07 16:45:53 -0800

    fixed trigram_new --> trigram

--- test_trigrams.py
+++ test_trigrams.py
@@ -30,35 +30,35 @@ n = 25
 
 def test_strip_d_hyphen():
     """Assert double hyphen is removed."""
-    from trigrams_new import strip_d_hyphen
+    from trigrams import strip_d_hyphen
     assert strip_d_hyphen(text) == text_cleaned
 
 
 def test_create_list():
     """Assert text string is converted to list of strings."""
-    from trigrams_new import create_list
+    from trigrams import create_list
     assert create_list(text_cleaned) == text_list
 
 
 def test_create_dict():
     """Assert dictionary of trigrams is created with tuple keys."""
-    from trigrams_new import create_dict
+    from trigrams import create_dict
     assert create_dict(text_list) == text_dict
 
 
 def test_seed_story():
     """Assert random story seed is generated."""
-    from trigrams_new import seed_story
+    from trigrams import seed_story
     assert len(seed_story(text_dict)) == 2
 
 
 def test_create_story_list():
     """Assert a story is returned as a list of strings."""
-    from trigrams_new import create_story_list
+    from trigrams import create_story_list
     assert len(create_story_list(text_dict, n)) == n
 
 
 def test_create_story():
     """Assert a string is returned (for testing)."""
-    from trigrams_new import create_story
+    from trigrams import create_story
     assert isinstance(create_story(story_list), str)
